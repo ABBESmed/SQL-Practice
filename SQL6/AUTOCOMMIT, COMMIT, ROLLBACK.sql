8e0871160fe238ccc1c4257f1891876b0c48630a
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-17 07:37:24 +0200

    autocommit commit rollback

---
+++ SQL6/AUTOCOMMIT, COMMIT, ROLLBACK.sql
@@ -0,0 +1,25 @@
+/*
+AUTOCOMMIT
+By default, MySQL automatically saves every change.
+*/
+SET AUTOCOMMIT = 1; -- Every INSERT, UPDATE, or DELETE is saved immediately.
+
+
+--To turn it off:
+SET AUTOCOMMIT = 0; -- Now MySQL waits until you decide to save or cancel.
+
+/*
+COMMIT
+COMMIT saves all changes permanently.
+*/
+--General syntax:
+COMMIT;
+
+--Example:
+SET AUTOCOMMIT = 0;
+
+UPDATE users
+SET age = 29
+WHERE id = 1;
+
+COMMIT; -- meaning: COMMIT = save the changes permanently.
