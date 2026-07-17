f3a514de75e28125539504fe1d79da3f2fb67e51
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-17 08:08:06 +0200

    unique

--- SQL8/uniqueconstraint.sql
+++ SQL8/uniqueconstraint.sql
@@ -4,3 +4,15 @@ CREATE TABLE users (
     id INT,
     email VARCHAR(100) UNIQUE
 );
+-- Now this is allowed:
+INSERT INTO users (id, email)
+VALUES (1, "mohammed@gmail.com");
+-- But this is not allowed:
+INSERT INTO users (id, email)
+VALUES (2, "mohammed@gmail.com");
+-- Because the email already exists.
+-- Easy memory: UNIQUE = no duplicate values
+
+-- You can also add it later:
+ALTER TABLE users
+ADD UNIQUE (email);
